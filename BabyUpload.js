77768d524617fbb40708fbea2d9ae48f1bf1fecf
JaxBBLL <showcye@foxmail.com>
2019-05-09 10:06:11 +0800

    f

--- BabyUpload.js
+++ BabyUpload.js
@@ -235,21 +235,5 @@
     }
   }
 
-
-
-  function dataURLtoFile(dataurl, filename) { //将base64转换为文件
-    var arr = dataurl.split(','),
-      mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]),
-      n = bstr.length,
-      u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new File([u8arr], filename, {
-      type: mime
-    });
-  }
-
   return Upload;
 }));
